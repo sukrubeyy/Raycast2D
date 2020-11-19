567c7ba7b0138128dd6c7f9e96c900b4164d21dd
sukrubeyy <sukru.beyy@outlook.com>
2020-11-19 16:22:03 +0300

    add scene and scripts

---
+++ Raycast2Dexample/Assets/gameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    RaycastHit2D hit;
+
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(Vector2.zero, Input.mousePosition);
+            if(hit!=null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
